c640c356221648311e8cc5692a88478748016406
wonseok-han <oshan1112@gmail.com>
2022-04-26 14:20:29 +0900

    historyData key 추가, history 화면 기본모양 fix

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,10 @@
 import Content from "components/Layout/Content";
+import { HISTORY_DATA } from "historyData";
 import Header from "components/Layout/Header";
+import { anime } from "components/Anime";
 import dynamic from "next/dynamic";
 import { getRandomColor } from "utils/randomColor";
+import { makeGuid } from "utils/functions";
 
 const Anime = dynamic(() => import("components/Anime"), {
   ssr: false,
@@ -16,9 +19,12 @@ const Home = () => {
           <Content>
             <Anime
               style={{
-                height: "1000px",
+                width: "100%",
+                height: "100%",
                 textAlign: "left",
-                overflow: "auto",
+                display: "grid",
+                gridTemplateColumns: "1fr 1fr",
+                gap: "2em",
               }}
               config={{
                 targets: "#vertical-bar",
@@ -30,11 +36,92 @@ const Home = () => {
               <div
                 id="vertical-bar"
                 style={{
+                  left: "49.5%",
                   width: "10px",
                   height: "100px",
                   backgroundColor: getRandomColor(),
+                  position: "fixed",
+                  borderRadius: "10px",
                 }}
-              ></div>
+              />
+
+              <div>
+                {HISTORY_DATA.map((item, index) => {
+                  const guid = makeGuid();
+                  const x = [-110, 15];
+                  const tansX = x[index % 2];
+
+                  return (
+                    <div key={item.key} className="history-items">
+                      <Anime
+                        config={{
+                          targets: `#${guid}`,
+                          opacity: 1,
+                          easing: "easeInOutQuad",
+                          delay: () => index * 300,
+                        }}
+                      >
+                        <div
+                          id={guid}
+                          style={{
+                            visibility: index % 2 === 0 ? "visible" : "hidden",
+                            opacity: 0,
+                            height: "100%",
+                            border: "2px solid black",
+                            marginBottom: "4rem",
+                            paddingLeft: "1rem",
+                            borderRadius: "10px",
+                          }}
+                        >
+                          <p>{item.name}</p>
+                          <p>{item.term}</p>
+                          <p>{item.role}</p>
+                          <p>{item.skill}</p>
+                        </div>
+                      </Anime>
+                    </div>
+                  );
+                })}
+              </div>
+
+              <div>
+                {HISTORY_DATA.map((item, index) => {
+                  const guid = makeGuid();
+                  const x = [-110, 15];
+                  const tansX = x[index % 2];
+
+                  return (
+                    <div key={item.key} className="history-items">
+                      <Anime
+                        config={{
+                          targets: `#${guid}`,
+                          opacity: 1,
+                          easing: "easeInOutQuad",
+                          delay: () => index * 300,
+                        }}
+                      >
+                        <div
+                          id={guid}
+                          style={{
+                            visibility: index % 2 !== 0 ? "visible" : "hidden",
+                            opacity: 0,
+                            height: "100%",
+                            border: "2px solid black",
+                            marginBottom: "4rem",
+                            paddingLeft: "1rem",
+                            borderRadius: "10px",
+                          }}
+                        >
+                          <p>{item.name}</p>
+                          <p>{item.term}</p>
+                          <p>{item.role}</p>
+                          <p>{item.skill}</p>
+                        </div>
+                      </Anime>
+                    </div>
+                  );
+                })}
+              </div>
             </Anime>
           </Content>
         </div>
@@ -45,6 +132,7 @@ const Home = () => {
         .background {
           background-color: #2D333B;
           height: 100%;
+          overflow: auto;
         }
         .container {
           // min-height: 100vh;
@@ -62,6 +150,9 @@ const Home = () => {
           border: 1px solid #eaeaea;
           border-radius: 10px;
           background-color: white;
+        }
+        .history-items {
+          // width: 50%;
         }
         `}
       </style>
